684b9dd2cb8c66e941f3c7c2a1eea5a2ce5b9b0a
Mustafa-ali20 <mustafaramakda61@gmail.com>
2025-05-31 17:52:18 +0300

    New changes in routes

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { getproducts } from "../store/reducers/ProductReducer";
 
 const Products = () => {
-  return (
-    <div>
-      Products
-    </div>
-  )
-}
+  const {} = useEffect;
 
-export default Products
-Products
+  return (
+    <div className=" bg-zinc-800 h-screen w-full p-10 flex">
+      <h1 className="text-2xl font-bold text-white flex-col">Product List</h1>
+      <ul className="flex flex-col">
+        <li>Hello </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Products;
--- src/store/reducers/ProductReducer.jsx
+++ src/store/reducers/ProductReducer.jsx
@@ -1,14 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit"; 
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
-    products: []
-}
- 
+  products: [],
+};
+
 export const ProductSlice = createSlice({
-     name: 'products',
+  name: "products",
   initialState,
-  reducers: {},
-   
-})
+  reducers: {
+    getproducts: (state, action) => {
+      axios
+        .get("https://fakestoreapi.com/products")
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+});
 
 export default ProductSlice.reducer;
+
+export const { getproducts } = ProductSlice.actions;
